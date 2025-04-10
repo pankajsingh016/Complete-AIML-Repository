e54572a972883ca582dc9b349b73810c104818ba
Pankaj Singh Kanyal <pankrj123@gmail.com>
2025-04-10 17:23:07 +0530

    Revision Python filehandling, exception handling,modules

---
+++ Python/Modules_importing/package/__init__.py
@@ -0,0 +1,1 @@
+'''This file __init__py is a special file in python that is used to define packages and initialize their namespace.'''
---
+++ Python/Modules_importing/test.py
@@ -0,0 +1,9 @@
+from package.math import addition, subtraction
+from package.math import *
+from package.subpackage.mult import multiply
+
+
+print(addition(2,3))
+print(subtraction(3,4))
+
+print(multiply(2,3))
